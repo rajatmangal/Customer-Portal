9d2286a6ff6dca2deedfdb4c15ec5e5e5bb17242
Rajat Mangal <rajatmangal@Rajats-MacBook-Pro.local>
2020-01-31 15:18:30 -0800

    Django structure added

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.http import HttpResponse
+
+
+urlpatterns = [
+    path('', views.home),
+    path('products/', views.products),
+    path('customers/', views.customers)
+]
