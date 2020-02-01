bcb4e1a932cf97f6a967076ec3227f2366e0e8af
Rajat Mangal <rajatmangal@Rajats-MacBook-Pro.local>
2020-01-31 16:34:11 -0800

    Customer, Orders and Products models added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name+"      "+self.phone+"      "+self.email
+
+
+class Product(models.Model):
+    category = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door')
+    )
+    name = models.CharField(max_length=200, null=True)
+    prize = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True,choices=category)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name+"      "+self.prize+"      "+self.category
+
+
+class Order(models.Model):
+    status = (
+        ('Pending', 'Pending'),
+        ('Out For Delivery', 'Out For Delivery'),
+        ('Delivered', 'Delivered')
+    )
+    #customer = models.CharField(max_length=200, null=True)
+    #product = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True,choices = status)
+
